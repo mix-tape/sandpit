5288b69051f4e8722c68f14fef16c16299a0c9fb
Ryan Berry <hey@mix-tape.io>
2016-09-05 15:19:23 +0800

    Give code some breathing room

--- base.php
+++ base.php
@@ -1,10 +1,22 @@
-<?php get_template_part('templates/head'); ?>
+<?php
+// ==========================================================================
+//
+//   Base layout
+//
+// ==========================================================================
+
+get_template_part('templates/head'); ?>
+
 <body <?php body_class(); ?>>
+
   <?php get_template_part('templates/header');?>
+
   <?php include roots_template_path(); ?>
+
   <?php get_template_part('templates/footer'); ?>
 
-  <?php if(get_option('googletagmanager')){?>
+  <?php if ( get_option('googletagmanager')) { ?>
+
   <!-- Google Tag Manager -->
   <noscript><iframe src="//www.googletagmanager.com/ns.html?id=<?php echo get_option('googletagmanager'); ?>"
   height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
@@ -14,6 +26,8 @@
   '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
   })(window,document,'script','dataLayer','<?php echo get_option('googletagmanager'); ?>');</script>
   <!-- End Google Tag Manager -->
+
   <?php } ?>
+
 </body>
 </html>
