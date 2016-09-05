b388466e9df7e9fe43669a0a2e8787fb055772d9
Ryan Berry <hey@mix-tape.io>
2016-09-05 15:50:23 +0800

    Properly enqueue assets via the enqueue API

--- templates/footer.php
+++ templates/footer.php
@@ -3,13 +3,4 @@
   <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
 </footer>
 
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<script>window.jQuery || document.write('<script src="<?php echo get_template_directory_uri(); ?>/assets/js/vendor/jquery-1.12.4.min.js"><\/script>')</script>
-<script src="<?php echo get_template_directory_uri(); ?>/assets/js/global.min.js"></script>
-
-<?php if(WP_DEBUG): ?>
-<!-- Grunt liverelaod -->
-<script src="//localhost:35729/livereload.js"></script>
-<?php endif; ?>
-
 <?php wp_footer(); ?>
