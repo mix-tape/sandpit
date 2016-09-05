2fcf5e51adedf902ddd454d297fbdf945db57eb5
Ryan Berry <hey@mix-tape.io>
2016-09-05 14:05:24 +0800

    Allow for custom code to be placed in tweaks directory and be globbed in

--- functions.php
+++ functions.php
@@ -8,8 +8,8 @@
 // ==========================================================================
 
 include_once('lib/configuration/initialize.php');           // Utility functions
-include_once('lib/configuration/activation.php');
-include_once('lib/configuration/assets-enqueue.php');
+include_once('lib/configuration/activation.php');           // Theme setup
+include_once('lib/configuration/assets-enqueue.php');       // Dependency management
 
 
 // --------------------------------------------------------------------------
@@ -32,20 +32,11 @@ foreach (glob(dirname(__FILE__) . "/lib/models/*.php") as $filename)
 }
 
 
-require_once locate_template('/lib/config.php');          // Configuration
-require_once locate_template('/lib/activation.php');      // Theme activation
-require_once locate_template('/lib/cleanup.php');         // Cleanup
-require_once locate_template('/lib/nav.php');             // Custom nav modifications
-require_once locate_template('/lib/htaccess.php');        // Rewrites for assets, H5BP .htaccess
-require_once locate_template('/lib/scripts.php');         // Scripts and stylesheets
-require_once locate_template('/lib/custom.php');          // Custom functions
+// --------------------------------------------------------------------------
+//   Include Tweaks
+// --------------------------------------------------------------------------
 
-/* ------------------------------------------------------------------------
-  Gravity form - enable hide label
------------------------------------------------------------------------- */
-add_filter("gform_enable_field_label_visibility_settings", "__return_true");
-
-/* ------------------------------------------------------------------------
-	Gravity form - go to anchor after form submit
------------------------------------------------------------------------- */
-add_filter("gform_confirmation_anchor", create_function("","return true;"));
+foreach (glob(dirname(__FILE__) . "/lib/tweaks/*.php") as $filename)
+{
+	include $filename;
+}
