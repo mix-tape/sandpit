f85a93db22ba4c4fe1faabef38bd6a7409a7f348
Ryan Berry <hey@mix-tape.io>
2016-09-06 19:44:30 +0800

    Cleanup head

--- templates/head.php
+++ templates/head.php
@@ -1,14 +1,23 @@
-<!DOCTYPE html>
+<!doctype html>
 <!--[if IE 8]>         <html class="no-js ie8down" <?php language_attributes(); ?>> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js" <?php language_attributes(); ?>> <!--<![endif]-->
 <head>
-  <meta charset="utf-8">
-  <title><?php wp_title(); ?></title>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta name="SKYPE_TOOLBAR" content="SKYPE_TOOLBAR_PARSER_COMPATIBLE" />
 
-  <?php wp_head(); ?>
-  <?php if (wp_count_posts()->publish > 0) : ?>
-  <link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name'); ?> Feed" href="<?php echo home_url(); ?>/feed/">
-  <?php endif; ?>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+<title><?php wp_title(); ?></title>
+<link rel="author" href="<?php bloginfo('url'); ?>/humans.txt" />
+
+<!-- Mobile specific -->
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+
+<!-- Start Wordpress Head -->
+<?php wp_head(); ?>
+<!-- End Wordpress Head -->
+
+<?php if (wp_count_posts()->publish > 0) : ?>
+<link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name'); ?> Feed" href="<?php echo home_url(); ?>/feed/">
+<?php endif; ?>
+
 </head>
