54ef794e2b4afdb64034ce00d65a47e07a0e6b30
Ryan Berry <hey@mix-tape.io>
2016-09-08 20:54:05 +0800

    De-queue vendor script

--- lib/configuration/assets-enqueue.php
+++ lib/configuration/assets-enqueue.php
@@ -15,8 +15,7 @@ function hibiki_enqueue_scripts() {
 
   // Queue Scripts
 
-  wp_enqueue_script( 'jquery', get_bloginfo('template_url') . '/assets/scripts/vendor.js', '', '', true ); // Name the vendor scripts jquery as this is often required by plugins
-  wp_enqueue_script( 'hibiki-scripts', get_bloginfo('template_url') . '/assets/scripts/global.js', 'jquery', '', true );
+  wp_enqueue_script( 'jquery', get_bloginfo('template_url') . '/assets/scripts/global.js', '', '', true ); // Name the vendor scripts jquery as this is often required by plugins
 
   // Queue Styles
 
