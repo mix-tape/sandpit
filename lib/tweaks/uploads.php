f323c3dd19f2ed5b7311fc1da073bdecc942f5b6
Ryan Berry <ryanberry@iinet.net.au>
2016-10-03 16:48:52 +0800

    Fix image url issues

--- lib/tweaks/uploads.php
+++ lib/tweaks/uploads.php
@@ -4,9 +4,11 @@
 //   Root Relative Upload Directory
 // --------------------------------------------------------------------------
 
-add_filter('upload_dir', 'hibiki_upload_directory');
+add_filter('wp_get_attachment_image_src', 'hibiki_simple_upload_directory');
 
-function hibiki_upload_directory( $param )
+function hibiki_simple_upload_directory( $image )
 {
-  return str_replace('wordpress/../', '', $param);
+  $image[0] = str_replace('wordpress/../', '', $image[0]);
+
+  return $image;
 }
