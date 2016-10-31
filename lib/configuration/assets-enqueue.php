17882537a35b49f13e8d4ccb03de3a796c1e5468
Ryan <hey@mix-tape.io>
2016-10-31 18:42:33 +0800

    Use hosted webfont.js to enqueue fonts async

--- lib/configuration/assets-enqueue.php
+++ lib/configuration/assets-enqueue.php
@@ -25,3 +25,29 @@ function hibiki_enqueue_scripts() {
 }
 
 if ( !is_admin()) add_action('wp_enqueue_scripts', 'hibiki_enqueue_scripts', 0 );
+
+
+// --------------------------------------------------------------------------
+//   Font enqueue
+// --------------------------------------------------------------------------
+
+function hibiki_enqueue_fonts() { ?>
+
+<script type="text/javascript">
+  WebFontConfig = {
+    // google: { families: [ 'Open+Sans' ] }
+  };
+  (function() {
+    var wf = document.createElement('script');
+    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+      '://ajax.googleapis.com/ajax/libs/webfont/1.6.16/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+  })();
+</script>
+
+<?php }
+
+add_action( 'wp_footer', 'hibiki_enqueue_fonts' );
