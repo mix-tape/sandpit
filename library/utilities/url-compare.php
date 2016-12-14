8c31160d759d64948646b9cebaeee1ae499c4aa6
Ryan <hey@mix-tape.io>
2016-12-14 14:10:07 +0800

    Update nav walker and add url_compare dependency

---
+++ library/utilities/url-compare.php
@@ -0,0 +1,41 @@
+<?php
+// --------------------------------------------------------------------------
+//
+//   Compare URL against relative URL
+//
+// --------------------------------------------------------------------------
+
+function url_compare($url, $rel) {
+  $url = trailingslashit($url);
+  $rel = trailingslashit($rel);
+  return ((strcasecmp($url, $rel) === 0) || root_relative_url($url) == $rel);
+}
+
+
+// --------------------------------------------------------------------------
+//
+//   Root Relative URLs
+//
+// --------------------------------------------------------------------------
+
+function root_relative_url($input) {
+  if (is_feed()) {
+    return $input;
+  }
+  $url = parse_url($input);
+  if (!isset($url['host']) || !isset($url['path'])) {
+    return $input;
+  }
+  $site_url = parse_url(network_home_url());  // falls back to home_url
+  if (!isset($url['scheme'])) {
+    $url['scheme'] = $site_url['scheme'];
+  }
+  $hosts_match = $site_url['host'] === $url['host'];
+  $schemes_match = $site_url['scheme'] === $url['scheme'];
+  $ports_exist = isset($site_url['port']) && isset($url['port']);
+  $ports_match = ($ports_exist) ? $site_url['port'] === $url['port'] : true;
+  if ($hosts_match && $schemes_match && $ports_match) {
+    return wp_make_link_relative($input);
+  }
+  return $input;
+}
